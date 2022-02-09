c853a97b3096acb145d20d0d8ff762c4350cf831
José Alberto Gómez García <modej@correo.ugr.es>
2022-02-09 11:38:02 +0100

    [#1] Seleccionar calle y mostrar canales HSV

--- main.py
+++ main.py
@@ -6,17 +6,27 @@ Created on Sun Jan 30 18:02:20 2022
 """
 
 import cv2
-from tkinter import Tk, filedialog
+from tkinter import Tk, filedialog, simpledialog
 
 Tk().withdraw()
-vid = filedialog.askopenfilename(initialdir = "./",title = "Seleccione fichero",filetypes = [
-    ("AVI file", ".avi"),                    
-    ("MP4 file", ".mp4"),
-    ("FLV file", ".flv"),
-])
-video = cv2.VideoCapture(vid)
+try:
+    vid = filedialog.askopenfilename(initialdir = "./",title = "Seleccione fichero",filetypes = [
+        ("AVI file", ".avi"),                    
+        ("MP4 file", ".mp4"),
+        ("FLV file", ".flv"),
+    ])
+except(OSError,FileNotFoundError):
+    print(f'No se ha podido abrir el fichero seleccionado.')
+except Exception as error:
+    print(f'Ha ocurrido un error: <{error}>')
 
-actual_frame = 0
+if vid is not None:
+    video = cv2.VideoCapture(vid)
+
+try:
+    lane = simpledialog.askinteger(title="Calle", prompt="Calle (1-8):", minvalue=1, maxvalue=8)
+except:
+    print(f'Número de calle inválido.')
 
 # Estadísticas sobre el vídeo
 FRAMES = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -24,13 +34,31 @@ fps = video.get(cv2.CAP_PROP_FPS)
 original_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 original_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))             
 
+# A diferenciar en función de la resolución del vídeo
+alto_calle = 120
+lane_xy = {"1": 690,"2": 590,"3": 490,"4": 390,"5": 290}    # Completar resto de calles
+top = lane_xy[str(lane)]
+bottom = top + alto_calle
+
+actual_frame = 0
+
 while video.isOpened():
 
     ok, frame = video.read()
-    
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)[...,1]     # Mostrar canal de saturación
-    cv2.imshow('Saturation from [HSV]', frame)
-    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+
+    hframe = frame[...,0]   # Mostrar canal de tono
+    hframe = hframe[top:bottom,:]
+    cv2.imshow('Hue from [HSV]', hframe)
+
+    sframe = frame[...,1]     # Mostrar canal de saturación
+    sframe = sframe[top:bottom,:]
+    cv2.imshow('Saturation from [HSV]', sframe)
+
+    vframe = frame[...,2]    # Mostrar canal de valor/brillo
+    vframe = vframe[top:bottom,:]
+    cv2.imshow('Value from [HSV]', vframe)
+
     actual_frame = actual_frame + 1
     
     # El bucle se ejecuta hasta el último frame o hasta que se presiona la tecla ESC
