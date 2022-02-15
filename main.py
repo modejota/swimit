60af0ee7be71901ad442b3300c17a89853d52457
José Alberto Gómez García <modej@correo.ugr.es>
2022-02-15 11:37:53 +0100

    OpenCV como cv2
    Seguir recomendación de no cambiar nombre

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created on Sun Jan 30 18:02:20 2022
 @author: modejota
 """
 
-import cv2 as cv
+import cv2
 from tkinter import Tk, filedialog, simpledialog
 
 
@@ -22,7 +22,7 @@ except Exception as error:
     print(f'Ha ocurrido un error: <{error}>')
 
 if vid is not None:
-    video = cv.VideoCapture(vid)
+    video = cv2.VideoCapture(vid)
 
 try:
     lane = simpledialog.askinteger(title="Calle", prompt="Calle (1-8):", minvalue=1, maxvalue=8)
@@ -31,10 +31,10 @@ except:
     lane = 3    # En el doc de Ángela dice que casi todas las calles 3 son buenas.
 
 # Estadísticas sobre el vídeo
-FRAMES = int(video.get(cv.CAP_PROP_FRAME_COUNT))
-fps = video.get(cv.CAP_PROP_FPS)
-original_width = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
-original_height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))             
+FRAMES = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = video.get(cv2.CAP_PROP_FPS)
+original_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+original_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))             
 
 # A diferenciar en función de la resolución del vídeo
 alto_calle = 120
@@ -45,24 +45,26 @@ bottom = top + alto_calle
 actual_frame = 0
 
 # Algoritmo GSOC (Google Summer of Code de 2017)
-background_subtr_GSOC = cv.bgsegm.createBackgroundSubtractorGSOC()
+background_subtr_GSOC = cv2.bgsegm.createBackgroundSubtractorGSOC()
 
 while video.isOpened():
 
     ok, frame = video.read()
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2YCrCb)[top:bottom,:][...,1] 
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)[top:bottom,:][...,1] 
     
-    cv.imshow('Cr from [YCrCb]', frame)
+    cv2.imshow('Banda Cr de YCrCb', frame)
 
     fg_gsoc = background_subtr_GSOC.apply(frame)
-    cv.imshow('FOREGROUND MASK GSOC',fg_gsoc)
+    cv2.imshow('Eliminacion de fondo con GSOC',fg_gsoc)
+
+    contours = cv2.findContours(fg_gsoc.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     actual_frame = actual_frame + 1
     
     # El bucle se ejecuta hasta el último frame o hasta que se presiona la tecla ESC
-    key = cv.waitKey(1) & 0xff
+    key = cv2.waitKey(1) & 0xff
     if actual_frame == FRAMES or key == 27: break
     
 video.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
 
