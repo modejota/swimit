94e24804b21aa84ae85a64a5a3aa5ae9a42a408c
José Alberto Gómez García <modej@correo.ugr.es>
2022-03-10 12:51:03 +0100

    Modifica constantes split
    Cambia inicializacion de cara a futuro

--- swimit/constants/split_constants.py
+++ swimit/constants/split_constants.py
@@ -1,7 +1,8 @@
 
 from dataclasses import dataclass
+from constants.resolution_constants import ResolutionValues
 
-@dataclass(frozen=True)
+@dataclass
 class SplitValues:
     """
     Objeto que almacenará las constantes relativas a los splits.
@@ -12,8 +13,21 @@ class SplitValues:
         Patrón para reconocer la longitud de la prueba, especificada en el título del vídeo.
     DISTANCE : int
         Distancia de un split en metros, correspondiente al largo de una piscina semiolímpica.
+    THRESHOLD_BRAZADAS: int
+        Número de frames mínimo entre dos posibles brazadas del nadador. Dependiente de la resolución del vídeo.
+    SPLIT_MIN_FRAMES: int
+        Número mínimo de frames que dura un split. Dependiente del frame rate del vídeo.
     """
 
     REGEX_PATTERN = '_([\d]*)m_'
-    DISTANCE = 25                 
+    DISTANCE = 25
+
+    def __init__(self,width,height,tipo_prueba,fps):
+        self.THRESHOLD_BRAZADAS = 0
+        self.SPLIT_MIN_FRAMES = 0
+        if tipo_prueba == "freestyle":
+            self.THRESHOLD_BRAZADAS = fps/2
+        if width == ResolutionValues.HALF_WIDTH and height == ResolutionValues.HALF_HEIGHT:
+            self.SPLIT_MIN_FRAMES = 350
+
 
