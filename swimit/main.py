2ae360e938e5a8fe336a541cd4d49ebd3aa0d9cb
José Alberto Gómez García <modej@correo.ugr.es>
2022-04-07 11:11:50 +0200

    [#11] Reescala y reduce framerate

--- swimit/main.py
+++ swimit/main.py
@@ -4,9 +4,10 @@ Created on Sun Jan 30 18:02:20 2022
 
 @author: modejota   |   José Alberto Gómez García
 """
-
-
+import math
 import sys
+import time
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,6 +17,7 @@ from auxiliary_functions import calculate_splits, countours_union
 from constants.pool_constants import PoolValues
 from constants.split_constants import SplitValues
 from constants.swimmer_constants import SwimmerValues
+from constants.resolution_constants import ResolutionValues
 
 Tk().withdraw()
 vid = None
@@ -50,12 +52,22 @@ fps = video.get(cv2.CAP_PROP_FPS)
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 actual_frame = 0
+current_frame = 0
 no_contour_detected = 0
 
 # Valores provenientes de las dataclasses
 PV = PoolValues()
 SWV = SwimmerValues()
 SPV = SplitValues("freestyle", fps)  # Testeando butterfly en verdad, temporal
+
+need_to_resize = False
+total_time = 0.0
+fps_factor = 1
+if width != ResolutionValues.HALF_WIDTH or height != ResolutionValues.HALF_HEIGHT:
+    need_to_resize = True
+if fps >= ResolutionValues.NORMAL_FRAME_RATE:
+    fps_factor = math.ceil(fps / ResolutionValues.NORMAL_FRAME_RATE)
+    frames = int(frames // fps_factor)
 
 # Get the bottom and top coordinates of a lane
 bottom = PV.LANES_Y.get(str(lane))
@@ -68,75 +80,86 @@ x_coordinates = np.full(frames, np.NaN)
 # Algoritmo GSOC (Google Summer of Code de 2017)
 background_subtr_GSOC = cv2.bgsegm.createBackgroundSubtractorGSOC()
 
+start_time = time.time()
 while video.isOpened():
 
     ok, frame = video.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)[bottom:top, :][..., 1]
-    cv2.imshow('Banda Cr de YCrCb', frame)
+    if ok:
+        # Reducir el framerate antes de procesar nada mas, para reducir tiempo de ejecucion y conservar memoria
+        if current_frame % fps_factor != 0:
+            current_frame += 1
+        else:
+            if need_to_resize:
+                frame = cv2.resize(frame, (ResolutionValues.HALF_WIDTH, ResolutionValues.HALF_HEIGHT))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)[bottom:top, :][..., 1]
 
-    fg_gsoc = background_subtr_GSOC.apply(frame)
+            fg_gsoc = background_subtr_GSOC.apply(frame)
 
-    contours = cv2.findContours(fg_gsoc, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
-    valid_contours = []
+            contours = cv2.findContours(fg_gsoc, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = contours[0] if len(contours) == 2 else contours[1]
+            valid_contours = []
 
-    for c in contours:
-        if SWV.SWIMMER_MIN_AREA <= cv2.contourArea(c) <= SWV.SWIMMER_MAX_AREA:
-            [x, y, w, h] = cv2.boundingRect(c)
-            # Discriminar corchera en base a su altura y obviar la zona del trampolin.
-            if h > PV.CORCHES_HEIGHT and x > PV.TRAMPOLIN_WIDTH and PV.MINIMUM_Y_ROI_LANE < y < PV.MAXIMUM_Y_ROI_LANE:
-                valid_contours.append(c)
+            for c in contours:
+                if SWV.SWIMMER_MIN_AREA <= cv2.contourArea(c) <= SWV.SWIMMER_MAX_AREA:
+                    [x, y, w, h] = cv2.boundingRect(c)
+                    # Discriminar corchera en base a su altura y obviar la zona del trampolin.
+                    if h > PV.CORCHES_HEIGHT and x > PV.TRAMPOLIN_WIDTH and \
+                            PV.MINIMUM_Y_ROI_LANE < y < PV.MAXIMUM_Y_ROI_LANE:
+                        valid_contours.append(c)
 
-    # Contornos por area descendente, el nadador será el más grande o la unión de los dos más grandes.
-    cntsSorted = sorted(valid_contours, key=lambda contour: cv2.contourArea(contour), reverse=True)
+            # Contornos por area descendente, el nadador será el más grande o la unión de los dos más grandes.
+            cntsSorted = sorted(valid_contours, key=lambda contour: cv2.contourArea(contour), reverse=True)
 
-    # Si tengo mas de dos contornos, me quedare con los dos de mayor área.
-    if len(cntsSorted) >= 2:
-        [x, y, w, h] = cv2.boundingRect(cntsSorted[0])
-        [x2, y2, w2, h2] = cv2.boundingRect(cntsSorted[1])
+            # Si tengo mas de dos contornos, me quedare con los dos de mayor área.
+            if len(cntsSorted) >= 2:
+                [x, y, w, h] = cv2.boundingRect(cntsSorted[0])
+                [x2, y2, w2, h2] = cv2.boundingRect(cntsSorted[1])
 
-        # Compruebo si dichos contornos corresponden a tronco y piernas del nadador.
-        if (abs(x - x2) < SWV.SWIMSUIT_MAX_WIDTH and
-                abs(y - y2) < SWV.SWIMMER_HEIGHT_DIFFERENCE and
-                w + w2 < SWV.SWIMMER_MAX_WIDTH):
-            [x, y, w, h] = countours_union([x, y, w, h], [x2, y2, w2, h2])
+                # Compruebo si dichos contornos corresponden a tronco y piernas del nadador.
+                if (abs(x - x2) < SWV.SWIMSUIT_MAX_WIDTH and
+                        abs(y - y2) < SWV.SWIMMER_HEIGHT_DIFFERENCE and
+                        w + w2 < SWV.SWIMMER_MAX_WIDTH):
+                    [x, y, w, h] = countours_union([x, y, w, h], [x2, y2, w2, h2])
 
-        # Si el contorno está en el área de interés, guardamos sus posiciones para posterior análsis.
-        if PV.MINIMUM_Y_ROI_LANE < y < PV.MAXIMUM_Y_ROI_LANE:
-            x_coordinates[actual_frame] = x
-            height_contour[actual_frame] = h
+                # Si el contorno está en el área de interés, guardamos sus posiciones para posterior análsis.
+                if PV.MINIMUM_Y_ROI_LANE < y < PV.MAXIMUM_Y_ROI_LANE:
+                    x_coordinates[actual_frame] = x
+                    height_contour[actual_frame] = h
 
-        cv2.rectangle(fg_gsoc, (x, y), (x + w, y + h), (255, 255, 255), 1)
+                cv2.rectangle(fg_gsoc, (x, y), (x + w, y + h), (255, 255, 255), 1)
 
-    # Si tengo un único contorno tengo al nadador perfectamente reconocido, o por lo menos el tronco superior.
-    elif len(cntsSorted) == 1:
-        [x, y, w, h] = cv2.boundingRect(cntsSorted[0])
-        cv2.rectangle(fg_gsoc, (x, y), (x + w, y + h), (255, 255, 255), 1)
+            # Si tengo un único contorno tengo al nadador perfectamente reconocido, o por lo menos el tronco superior.
+            elif len(cntsSorted) == 1:
+                [x, y, w, h] = cv2.boundingRect(cntsSorted[0])
+                cv2.rectangle(fg_gsoc, (x, y), (x + w, y + h), (255, 255, 255), 1)
 
-        # Si el contorno está en el área de interés, guardamos sus posiciones para posterior análsis.
-        if PV.MINIMUM_Y_ROI_LANE < y < PV.MAXIMUM_Y_ROI_LANE:
-            x_coordinates[actual_frame] = x
-            height_contour[actual_frame] = h
+                # Si el contorno está en el área de interés, guardamos sus posiciones para posterior análsis.
+                if PV.MINIMUM_Y_ROI_LANE < y < PV.MAXIMUM_Y_ROI_LANE:
+                    x_coordinates[actual_frame] = x
+                    height_contour[actual_frame] = h
 
-    else:
-        no_contour_detected += 1
+            else:
+                no_contour_detected += 1
 
-    cv2.imshow('Contornos tras procesamiento', fg_gsoc)
-
-    actual_frame += 1
-
-    key = cv2.waitKey(1) & 0xff
-    # El bucle se pausa al pulsar P, y se reanuda al pulsar cualquier otra tecla
-    if key == 112:
-        cv2.waitKey(-1)
-    # El bucle se ejecuta hasta el último frame o hasta que se presiona la tecla ESC
-    if actual_frame == frames or key == 27:
-        break
+            # cv2.imshow('Contornos tras procesamiento', fg_gsoc)
+            actual_frame += 1
+            current_frame += 1
+        print(f'\rProcesando frame {current_frame} de {frames}.', end='')
+        key = cv2.waitKey(1) & 0xff
+        # El bucle se pausa al pulsar P, y se reanuda al pulsar cualquier otra tecla
+        if key == 112:
+            cv2.waitKey(-1)
+        # El bucle se ejecuta hasta el último frame o hasta que se presiona la tecla ESC
+        if current_frame == frames or key == 27:
+            break
 
 # Estadísticas y obtención de resultados
-print('Frames sin contornos detectados: %d' % no_contour_detected)
+print('\nFrames sin contornos detectados: %d' % no_contour_detected)
+print('Tiempo total de ejecución: %.2f segundos' % (time.time() - start_time))
+
 
 # 1. Procesar coordenadas en las que no se detectó al nadador.
+# Problema con los índices, se me cuela por uno.
 x_coordinates_detected = np.copy(x_coordinates)
 height_contour_detected = np.copy(height_contour)
 for i in range(0, frames):
@@ -159,21 +182,22 @@ peaks_I, _ = find_peaks(sentidoS * -1, distance=SPV.SPLIT_MIN_FRAMES, width=11)
 
 indexes = []
 peaks_coordinates_sentido = list(sentido[p] for p in peaks) + list(sentido[p] for p in peaks_I)
+first_index = np.where(x_coordinates_detected > PV.AFTER_JUMPING_X)[0][0]
+indexes.append(first_index)
 for i, x in enumerate(x_coordinates):
     if x in peaks_coordinates_sentido and \
             not any(t in indexes for t in range(i - SPV.SPLIT_MIN_FRAMES // 2, i + SPV.SPLIT_MIN_FRAMES // 2)):
         indexes.append(i)
-first_index = np.where(x_coordinates_detected > PV.AFTER_JUMPING_X)[0][0]
 last_index = np.where(x_coordinates_detected > 0)[0][-1]
-indexes = np.append(np.insert(np.sort(indexes), 0, first_index), last_index)
+indexes = np.append(np.sort(indexes), last_index)
 
 brazadas_min_total = 0.0
 x_axis = np.arange(0, frames)
 # 3. Cálculos en función del split.
 for i in range(1, splits + 1):
     # 3.1- Extraer las coordenadas X y alturas del split en cuestión.
-    xs = x_coordinates_detected[indexes[i-1]:indexes[i]]
-    hs = height_contour_detected[indexes[i-1]:indexes[i]]
+    xs = x_coordinates_detected[indexes[i - 1]:indexes[i]]
+    hs = height_contour_detected[indexes[i - 1]:indexes[i]]
     # 3.2 Esteblecer los frames extremos de la región de interés.
     if i == 1:  # Primer split (izquierda a derecha)
         first_index = xs[np.where(xs >= PV.AFTER_JUMPING_X)[0][0]]
@@ -195,8 +219,8 @@ for i in range(1, splits + 1):
 
     magnitud = [hs_suavizado[true_peaks[i]] for i in range(0, len(true_peaks))]
     plt.figure()
-    plt.plot(x_axis[indexes[i-1]:indexes[i]], hs_suavizado, label="H Suavizada")
-    plt.plot(x_axis[indexes[i-1]:indexes[i]][true_peaks], magnitud, marker="o", ls="", ms=3, label='Peak')
+    plt.plot(x_axis[indexes[i - 1]:indexes[i]], hs_suavizado, label="H Suavizada")
+    plt.plot(x_axis[indexes[i - 1]:indexes[i]][true_peaks], magnitud, marker="o", ls="", ms=3, label='Peak')
     plt.title("Alturas y brazadas en split %d" % i)
     plt.ylabel("Altura (pixeles)")
     plt.xlabel("Frame")
